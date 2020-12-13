69157aacf626481109a328039bd23f1ecb178c73
hduprat <hugod@bam.tech>
2020-12-13 14:04:50 +0100

    :sparkles: (exercise 13) part two

--- 13.ts
+++ 13.ts
@@ -2,11 +2,21 @@ import { text, lineBreak, title, result } from "./utils/console";
 import { colors, modifiers } from "./utils/consoleColors";
 import { getLinesOfFile } from "./utils/getLinesOfFile";
 
-const getBusLines = (input: string): number[] =>
-  input
-    .split(",")
-    .filter((id) => id !== "x")
-    .map((id) => parseInt(id));
+const getBusLines = (
+  input: string
+): { busLines: number[]; indexes: number[] } => {
+  const busLineArray = input.split(",");
+  const busLines: number[] = [];
+  const indexes: number[] = [];
+  busLineArray.forEach((line, index) => {
+    if (line !== "x") {
+      busLines.push(parseInt(line));
+      indexes.push(index);
+    }
+  });
+
+  return { busLines, indexes };
+};
 
 const findEarliestDepartureTimeAndLine = (
   busLines: number[],
@@ -22,10 +32,45 @@ const findEarliestDepartureTimeAndLine = (
   return { line, timestamp };
 };
 
+let loopCount = 0;
+
+const playContest = (busLines: number[], indexes: number[]): number => {
+  let n = 1;
+  let time = 0;
+  let step = busLines[indexes[0]];
+  while (true) {
+    loopCount++;
+    time += step;
+    text(time, "step=", step);
+    if ((time + indexes[n]) % busLines[n] !== 0) {
+      text(
+        time + indexes[n],
+        "is",
+        colors.red,
+        "NOT divisible by",
+        busLines[n],
+        modifiers.reset
+      );
+      continue;
+    }
+    text(
+      time + indexes[n],
+      "is",
+      colors.green,
+      "divisible by",
+      busLines[n],
+      modifiers.reset
+    );
+    step *= busLines[n];
+    n++;
+    if (n === busLines.length) return time;
+  }
+};
+
 const playScenario = async (path: string) => {
   const lines = await getLinesOfFile(path);
   const arrivalTimestamp = parseInt(lines[0]);
-  const busLines = getBusLines(lines[1]);
+  const { busLines, indexes } = getBusLines(lines[1]);
 
   text("Available bus lines", busLines);
 
@@ -48,6 +93,15 @@ const playScenario = async (path: string) => {
     "Multiplication result:",
     (departureTimestamp - arrivalTimestamp) * departureLine
   );
+  lineBreak();
+
+  title(`Second exercise: win the contest!`, "green");
+  const winningTime = playContest(busLines, indexes);
+  result(
+    `The winning time for  ${colors.magenta}${lines[1]}${modifiers.reset} is`,
+    winningTime
+  );
+  text("Total loop count", loopCount);
   lineBreak();
 };
 
