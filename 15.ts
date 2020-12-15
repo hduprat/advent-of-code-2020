3350513a8d441852f8f6ec5313112db81f20feac
hduprat <hugod@bam.tech>
2020-12-15 10:53:56 +0100

    :sparkles: (exercise 15) part two

--- 15.ts
+++ 15.ts
@@ -1,26 +1,25 @@
 import { text, lineBreak, title, result } from "./utils/console";
 import { getLinesOfFile } from "./utils/getLinesOfFile";
 
-interface Memory {
-  [n: number]: number;
-}
+type Memory = Map<number, number>;
 
-const playMemoryGame = (input: string): number => {
+const playMemoryGame = (input: string, finalTurn: number): number => {
   const startingNumbers = input.split(",");
-  const memory = startingNumbers
-    .slice(0, startingNumbers.length - 1)
-    .reduce((mem, n, i) => {
-      text("The spoken number at turn", i + 1, "is", parseInt(n));
-      return { ...mem, [n]: i };
-    }, {} as Memory);
+  const memory = new Map<number, number>();
+  startingNumbers.slice(0, startingNumbers.length - 1).forEach((n, i) => {
+    text("The spoken number at turn", i + 1, "is", parseInt(n));
+    memory.set(parseInt(n), i);
+  });
   let spokenNumber: number = parseInt(
     startingNumbers[startingNumbers.length - 1]
   );
-  for (let turn = startingNumbers.length - 1; turn < 2019; turn++) {
-    text("The spoken number at turn", turn + 1, "is", spokenNumber);
-    const newNumber =
-      memory[spokenNumber] === undefined ? 0 : turn - memory[spokenNumber];
-    memory[spokenNumber] = turn;
+  for (let turn = startingNumbers.length - 1; turn < finalTurn - 1; turn++) {
+    if (turn + 1 < 3000 || (turn + 1) % 1000000 === 0)
+      text("The spoken number at turn", turn + 1, "is", spokenNumber);
+    const newNumber = memory.has(spokenNumber)
+      ? turn - memory.get(spokenNumber)
+      : 0;
+    memory.set(spokenNumber, turn);
     spokenNumber = newNumber;
   }
 
@@ -34,8 +33,16 @@ const playScenario = async (path: string) => {
     `First exercise: say the 2020th number spoken at the memory game.`,
     "green"
   );
-  const n = playMemoryGame(lines[0]);
+  const n = playMemoryGame(lines[0], 2020);
   result("The 2020th spoken number is", n);
+  lineBreak();
+
+  title(
+    `Second exercise: say the 30000000th number spoken at the memory game.`,
+    "green"
+  );
+  const p = playMemoryGame(lines[0], 30000000);
+  result("The 30000000th spoken number is", p);
   lineBreak();
 };
 
